b5bae3b0ea8d5d73c32544421e7292435bc36eb1
RTV <ricktenvoorde91@gmail.com>
2025-01-25 19:48:08 +0100

    Initial commit

--- client/src/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ClerkProvider } from '@clerk/clerk-react';
-import './index.css';
-import App from './App';
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx(ClerkProvider, { publishableKey: PUBLISHABLE_KEY, children: _jsx(App, {}) }) }));
